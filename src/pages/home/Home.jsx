d72254d3ce9d5a32577b56a2f91798bb1a988805
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-02-05 15:19:22 -0500

    worked and fixed the layout on the homepage and styling for the button and the page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Profile from "../../assets/home.jpg";
+import { Link } from "react-router-dom";
+import { TiArrowRight } from "react-icons/ti";
+import "./home.css"
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home section grid">
+      <img src={Profile} alt="" className="home__img" />
 
-export default Home
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I'm Nawfal Sentissi</span> Web Developer
+          </h1>
+
+          <p className="home__description">
+            I'm a Web designer & front-end developer focused on crafting clean &
+            user-friendly experiences, I am passionate about building excellent
+            software that improves the lives of those around me.
+          </p>
+
+          <Link to="/about" className="button">
+            More about me{" "}
+            <span className="button__icon">
+            <TiArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color__block"></div>
+    </section>
+  );
+};
+
+export default Home;
