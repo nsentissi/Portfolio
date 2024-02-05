8cbacbcbe21b5f6ff8abd20ad6e48568f41d3018
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-02-05 09:40:44 -0500

    created pages, components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import  React from 'react'
-
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Portfolio from './pages/portfolio /Portfolio'
+import Contact from './pages/contact/contact'
+
 
 function App() {
  
 
   return (
     <>
-      
+      <Navbar/>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='about' element ={<About/>}/>
+        <Route path='portfolio' element ={<Portfolio/>}/>
+        <Route path='contact' element ={<Contact/>}/>
+      </Routes>
     </>
   )
 }
