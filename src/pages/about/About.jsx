60a7961a1f35c9252324df51bee8b1c31f336f32
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-02-06 12:40:22 -0500

    completed the info and stats sections

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Info from "../../components/info";
+import Stats from "../../components/Stats";
+import { FaDownload } from "react-icons/fa";
+import CV from "../../assets/Nawfal-Sentissi-CV.pdf";
+import "./about.css"
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
-  )
-}
+    <main className="section container">
+      <section className="about">
+        <h2 className="section__title">
+          About <span>Me</span>
+        </h2>
+        <div className="about__container grid">
+          <div className="about__info">
+            <h3 className="section__subtitle">Personal Infos</h3>
 
-export default About
+            <ul className="info__list grid">
+              <Info />
+            </ul>
+
+            <a href={CV} download="" className="button">
+              Download CV
+              <span className="button__icon">
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+          <div className="stats grid">
+            <Stats/>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default About;
