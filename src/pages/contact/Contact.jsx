eaabacbdcb8fae31960b56a1f1202955d6ea5547
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-02-06 21:07:46 -0500

    fixed contact form for my email

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,145 @@
-import React from 'react'
+import React from "react";
+import {
+  FaEnvelopeOpen,
+  FaPhoneSquareAlt,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+import { FiSend } from "react-icons/fi";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./contact.css";
 
 const Contact = () => {
-  return (
-    <div>
-      Contact
-    </div>
-  )
-}
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-export default Contact
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICEID,
+        import.meta.env.VITE_EMAILJS_TEMPLATEID,
+        e.target,
+        import.meta.env.VITE_EMAILJS_USERID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast.success("Message sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+          toast.error("Failed to send the message, please try again.");
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <section className="contact section">
+      <h2 className="section__title">
+        Get In <span>Touch</span>
+      </h2>
+
+      <div className="contact__container container grid">
+        <div className="contact__data">
+          <h3 className="contact__title">Let's Connect</h3>
+
+          <p className="contact__description">
+            Feel free to get in touch with me. I am always open to discussing
+            new projects, creative ideas or opportunities to be part of your
+            visions.
+          </p>
+
+          <div className="contact__info">
+            <div className="info__item">
+              <FaEnvelopeOpen className="info__icon" />
+
+              <div>
+                <span className="info__title">Email me</span>
+                <h4 className="info__desc">sentissinawfal@gmail.com</h4>
+              </div>
+            </div>
+
+            <div className="info__item">
+              <FaPhoneSquareAlt className="info__icon" />
+
+              <div>
+                <span className="info__title">Call me</span>
+                <h4 className="info__desc">+49 1573 3917793</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="contact__socials">
+            <a
+              href="https://linkedin.com/in/nawfal-sentissi"
+              target="_blank"
+              className="contact__social-link"
+            >
+              <FaLinkedin />
+            </a>
+
+            <a
+              href="https://github.com/nsentissi"
+              target="_blank"
+              className="contact__social-link"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+
+        <form className="contact__form" onSubmit={sendEmail}>
+          <div className="form__input-group">
+            <div className="form__input-div">
+              <input
+                type="text"
+                name="from_name"
+                placeholder="Your Name"
+                className="form__control"
+              />
+            </div>
+
+            <div className="form__input-div">
+              <input
+                type="email"
+                name="from_email"
+                placeholder="Your Email"
+                className="form__control"
+              />
+            </div>
+
+            <div className="form__input-div">
+              <input
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                className="form__control"
+              />
+            </div>
+          </div>
+
+          <div className="form__input-div">
+            <textarea
+              placeholder="Your message"
+              name="message"
+              className="form__control textarea"
+            ></textarea>
+          </div>
+
+          <button className="button">
+            Send Message
+            <span className="button__icon contact__button-icon">
+              <FiSend />
+            </span>
+          </button>
+          <ToastContainer />
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
