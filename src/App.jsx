de9bcefd6788ab19181663e5dc5df9444bc8f0b1
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-03-04 13:03:26 +0100

    degugged path

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Portfolio from './pages/portfolio /Portfolio'
-import Contact from './pages/contact/contact'
+import Contact from './pages/contact/Contact'
 import Themes from './components/Themes'
 
 
