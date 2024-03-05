1f15cd7465e8931264346331d2632aa54120d0fd
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-03-05 11:55:00 +0100

    edited projects information

--- src/data.jsx
+++ src/data.jsx
@@ -11,9 +11,8 @@ import { FiFileText, FiUser, FiExternalLink } from "react-icons/fi";
 import Work1 from "./assets/project-1.png";
 import Work2 from "./assets/project-2.png";
 import Work3 from "./assets/project-3.png";
-import Work4 from "./assets/project-4.jpeg";
-import Work5 from "./assets/project-5.jpg";
-import Work6 from "./assets/project-6.jpg";
+import Work4 from "./assets/project-4.png"
+
 
 import Theme1 from "./assets/purple.png";
 import Theme2 from "./assets/red.png";
@@ -231,7 +230,7 @@ export const portfolio = [
     id: 2,
     img: Work2,
     title: "Pokedex",
-    description: "Full-stack Pokemon battle simulator built with the MERN stack. Features dynamic Pokemon filtering, engaging battle mechanics, and a persistent leaderboard to track top trainers.",
+    description: "Full-stack Pokemon battle simulator built with the MERN stack. Features dynamic Pokemon filtering, engaging battle mechanics, and a leaderboard to track top pokemons.",
     tech: "React, Node.js, Express, MongoDB, CSS.",
     links: [
      
@@ -250,18 +249,18 @@ export const portfolio = [
   {
     id: 3,
     img: Work3,
-    title: "E-commerce",
-    description: "Gained essential React skills building a product-focused app. Implemented dynamic data fetching, filtering logic, and component-based structures.",
-    tech: "React CSS.",
+    title: "Airbnb-clone",
+    description: "Crafted an Airbnb clone with the MERN stack, enabling user authentication, accommodation listings, and bookings. Focused on dynamic interactions, real-time updates, and a seamless, component-driven UI for an intuitive booking experience",
+    tech: "React, Node.js, Express, MongoDB, TailwindCss",
     links: [
      
       {
         icon: <FiExternalLink />,
-        desc: "https://pokefight-044.netlify.app/",
+        desc: "",
       },
       {
         icon: <FaGithub/>,
-        desc: "https://github.com/nsentissi/ecommerce-react.git"
+        desc: "https://github.com/nsentissi/Airbnb-clone"
       
       }
     ],
@@ -270,82 +269,24 @@ export const portfolio = [
   {
     id: 4,
     img: Work4,
-    title: "Video Editing",
-    details: [
-      {
-        icon: <FiFileText />,
-        title: "Project : ",
-        desc: "Video",
-      },
-      {
-        icon: <FiUser />,
-        title: "Client : ",
-        desc: "Dribble",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language : ",
-        desc: "Adobe Premium",
-      },
+    title: "Portfolio",
+    description: "Explore my sleek, minimalist portfolio website, featuring a smooth dark mode and style switcher for a personalized browsing experience. It's designed to showcase my web development projects and skills in a clean, user-friendly environment",
+    tech: "React, CSS",
+    links: [
+     
       {
         icon: <FiExternalLink />,
-        title: "Preview : ",
-        desc: "www.dribble.com",
+        desc: "https://nsentissi.netlify.app/",
       },
+      {
+        icon: <FaGithub/>,
+        desc: "https://github.com/nsentissi/Portfolio"
+      
+      }
     ],
   },
 
-  {
-    id: 5,
-    img: Work5,
-    title: "Landing Page",
-    details: [
-      {
-        title: "Project : ",
-        desc: "Website",
-      },
-      {
-        title: "Client : ",
-        desc: "Dribble",
-      },
-      {
-        title: "Language : ",
-        desc: "React JS, Node JS",
-      },
-      {
-        title: "Preview : ",
-        desc: "www.dribble.com",
-      },
-    ],
-  },
-
-  {
-    id: 6,
-    img: Work6,
-    title: "Photo Editing",
-    details: [
-      {
-        icon: <FiFileText />,
-        title: "Project : ",
-        desc: "Photo",
-      },
-      {
-        icon: <FiUser />,
-        title: "Client : ",
-        desc: "Dribble",
-      },
-      {
-        icon: <FaCode />,
-        title: "Language : ",
-        desc: "Adobe Photoshop",
-      },
-      {
-        icon: <FiExternalLink />,
-        title: "Preview : ",
-        desc: "www.dibble.com",
-      },
-    ],
-  },
+  
 ];
 
 export const themes = [
